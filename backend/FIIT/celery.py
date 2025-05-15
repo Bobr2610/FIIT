f2326a40216340f3db5a59dab15eb694103f316b
EldarSalmanow <eldar.salm@gmail.com>
2025-05-15 18:59:47 +0300

    adding celery task

--- backend/FIIT/celery.py
+++ backend/FIIT/celery.py
@@ -7,3 +7,13 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FIIT.settings')
 app = Celery('FIIT')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender: Celery, **kwargs):
+    sender.add_periodic_task(300.0, update_currencies.s(), name='update currencies')
+
+
+@app.task
+def update_currencies():
+    print('TODO: !!!Hello!!!')
